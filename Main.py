de9ac7585d07483c38b5dd7d8f29c8d6c00f7d45
Alvaro Robles <roblesgg16@gmail.com>
2025-11-02 19:40:25 +0100

    Puesta a punto de proyecto
    Creo el archivo: Controller, UI, Main

---
+++ Main.py
@@ -0,0 +1,17 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow
+from Controller import Ui_MainWindow 
+
+class MiVentana(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        
+        self.ui = Ui_MainWindow() 
+        
+        self.ui.setupUi(self)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    ventana = MiVentana()
+    ventana.show()
+    sys.exit(app.exec_())
