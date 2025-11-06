7def5bed290ecd94b7793a52fbb64bcd9de623fe
Alvaro Robles <roblesgg16@gmail.com>
2025-11-06 10:33:43 +0100

    Main

--- Main.py
+++ Main.py
@@ -1,14 +1,20 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QDialog
+
 from ControllerCrudEvento import Ui_MainWindow 
+
+from CrearEvento1Controller import Ui_DialogoCrearEvento
+
 
 class MiVentana(QMainWindow):
     def __init__(self):
         super().__init__()
         
         self.ui = Ui_MainWindow() 
-        
         self.ui.setupUi(self)
+        self.ui.btnCrearEvento.clicked.connect(self.abrir_crearEvento1)
+        
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
